02d8f29220b4bf6899d00c74ff01254aac095b9f
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-11-17 23:47:13 +0900

    test :how to add event nextjs

--- src/app/test/inside/page.tsx
+++ src/app/test/inside/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+import { useState } from "react";
 import testData from "./data";
 const Inside = () => {
-  return <div>{testData}</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      {testData}
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
 };
 export default Inside;
