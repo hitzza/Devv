6b56b2ebfc04c892d8ab866502fc0be22ab65a21
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-11-21 17:10:22 +0900

    init : db url 환경 변수로 변경

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,14 +1,12 @@
 import { MongoClient } from "mongodb";
-const url =
-  "mongodb+srv://cslzh4433:skarlgks8@devv.vnf1tdr.mongodb.net/?retryWrites=true&w=majority";
 let connectDB;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url).connect();
+    global._mongo = new MongoClient(process.env.REACT_APP_DBURL).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url).connect();
+  connectDB = new MongoClient(process.env.REACT_APP_DBURL).connect();
 }
 export { connectDB };
